94a94245da72584d370b5587e2194de05460bae0
akisatoon <akisatoon@gmail.com>
2025-01-31 18:27:05 +0900

    model: validation and update check

--- app/models/celebrity.rb
+++ app/models/celebrity.rb
@@ -1,2 +1,14 @@
 class Celebrity < ApplicationRecord
+    validates :name, :image_url, presence: true
+    validates :known_cnt, :unknown_cnt, numericality: { only_integer: true }
+
+    before_update :prevent_name_image_update
+    
+    private
+    def prevent_name_image_update
+        if name_changed? || image_url_changed?
+            errors.add(:base, "name and image_url cannot be updated")
+            throw :abort
+        end
+    end
 end
